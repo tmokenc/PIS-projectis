e5fde4a070fc50a4c015e1c661773bbe9cd481fe
Nguyen Le Duy <xnguye27@stud.fit.vut.cz>
2026-03-15 01:22:48 +0100

    feat: add graceful shutdown

--- services/notification-service-rust/src/main.rs
+++ services/notification-service-rust/src/main.rs
@@ -6,6 +6,7 @@ use opentelemetry_sdk::{trace::SdkTracerProvider, Resource};
 use prost_types::Timestamp;
 use serde::{Deserialize, Serialize};
 use std::{env, sync::Arc};
+use tokio::signal;
 use tonic::{transport::Server, Request, Response, Status};
 use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt, EnvFilter};
 
@@ -229,8 +230,34 @@ async fn main() -> anyhow::Result<()> {
 
     Server::builder()
         .add_service(NotificationServiceServer::new(NotificationGrpc { store }))
-        .serve(addr)
+        .serve_with_shutdown(addr, shutdown_signal())
         .await?;
 
     Ok(())
+}
+
+// Samelessly taken from axum's graceful shutdown example:
+// https://github.com/tokio-rs/axum/blob/da26db264f811e73485f1db1c134d374e8f99464/examples/graceful-shutdown/src/main.rs#L54
+async fn shutdown_signal() {
+    let ctrl_c = async {
+        signal::ctrl_c()
+            .await
+            .expect("failed to install Ctrl+C handler");
+    };
+
+    #[cfg(unix)]
+    let terminate = async {
+        signal::unix::signal(signal::unix::SignalKind::terminate())
+            .expect("failed to install signal handler")
+            .recv()
+            .await;
+    };
+
+    #[cfg(not(unix))]
+    let terminate = std::future::pending::<()>();
+
+    tokio::select! {
+        _ = ctrl_c => {},
+        _ = terminate => {},
+    }
 }
